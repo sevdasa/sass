9162c5a23e474460a6f3dc9925c1b0cd800c77f3
sevda <sevdasazdar@gmail.com>
2020-04-12 18:00:48 +0430

    first sass and js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+function myFunction() {
+    var x = document.getElementById("myMobnav");
+    if (x.className === "nav-item") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-item";
+    }
+  }
+
+
+
+
+
+// var mob = document.getElementsByClassName("mob-nav");
+// var btn = document.getElementsByClassName("icon")[0];
+// function myFunction(){
+    
+//     if(mob.style.display === "flex"){
+//     var contain = document.getElementsByClassName(mob-item)[0];
+//         contain.style.display === flex;
+//     }else{
+//         contain.style.display === none;
+
+//     }
+
+
+// btn.addEventListener("click", show);
+// btn.addEventListener("click", hide);
+// function show(){
+//     if(btn.onclick()){
+//         mob.style.display === "flex";
+
+//     }
+// }
+// function hide(){
+//     mob.style.display === "none";
+
+// }
+// }
